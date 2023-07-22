e0fa583e2ea50c76c7978c054658562454c7f5e6
zamzami16 <yusufazzamzami@gmail.com>
2023-07-22 15:23:28 +0700

    modified:   rukem/asgi.py 	modified:   vercel.json

--- rukem/asgi.py
+++ rukem/asgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rukem.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rukem.settings")
 
 application = get_asgi_application()
+
+app = application
